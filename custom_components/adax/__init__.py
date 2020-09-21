8010fb43ec4d23977f44b86f15e425c7bb5482ac
Daniel Hjelseth Høyer <mail@dahoiv.net>
2020-09-21 22:11:20 +0200

    Black (#9)
    * Update black.yaml
    
    * Automated changes
    
    * remove unused import
    
    * style
    
    * Automated changes

--- custom_components/adax/__init__.py
+++ custom_components/adax/__init__.py
@@ -1,15 +1,10 @@
 """The Adax heater integration."""
-import logging
 
-_LOGGER = logging.getLogger(__name__)
-
-from homeassistant.const import (
-    CONF_PASSWORD,
-)
 
 async def async_setup(hass, config):
     """Set up the Adax platform."""
     return True
+
 
 async def async_setup_entry(hass, entry):
     """Set up the Adax heater."""
@@ -19,10 +14,10 @@ async def async_setup_entry(hass, entry):
     )
     return True
 
+
 async def async_unload_entry(hass, config_entry):
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_forward_entry_unload(
         config_entry, "climate"
     )
     return unload_ok
-
